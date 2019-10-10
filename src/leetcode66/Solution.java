701098368ecca17c6a50d757e213c72490478573
Hedy2728 <Haomeng2728@163.com>
2019-10-10 11:22:48 +0800

    for leetcode66

--- src/leetcode66/Solution.java
+++ src/leetcode66/Solution.java
@@ -1,4 +1,26 @@
 package leetcode66;
 
+import java.util.Arrays;
+
 public class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            }
+            else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] res = {9};
+        System.out.println(Arrays.toString(s.plusOne(res)));
+    }
 }
