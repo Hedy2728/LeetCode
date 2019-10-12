11216c7114e6d37fef7558ab753d14f7426bde43
Hedy2728 <Haomeng2728@163.com>
2019-10-12 15:42:17 +0800

    for leetcode69

---
+++ src/leetcode69/Solution.java
@@ -0,0 +1,13 @@
+package leetcode69;
+
+public class Solution {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.mySqrt(9));
+    }
+}
