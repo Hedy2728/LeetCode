d15d2cb3a71d12a8cf755a34c00c6aca39960628
Hedy2728 <Haomeng2728@163.com>
2019-10-31 21:39:36 +0800

    for leetcode110

---
+++ src/leetcode110/Solution.java
@@ -0,0 +1,27 @@
+package leetcode110;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}
+
+public class Solution
+{
+    public boolean isBalanced(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        return Math.abs(Height(root.left) - Height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int Height(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        return Math.max(Height(root.left), Height(root.right)) + 1;
+    }
+
+}
