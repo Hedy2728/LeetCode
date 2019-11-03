7fc60a0382438df01984ac4c093787cf85576c21
Hedy2728 <Haomeng2728@163.com>
2019-11-03 20:46:12 +0800

    for leetcode111

---
+++ src/leetcode111/Solution.java
@@ -0,0 +1,31 @@
+package leetcode111;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if((root.left == null) && (root.right == null)){
+            return 1;
+        }
+        int min = Integer.MAX_VALUE;
+
+        if (root.left != null) {
+            min = Math.min(minDepth(root.left), min);
+        }
+
+        if (root.right != null) {
+            min = Math.min(minDepth(root.right), min);
+        }
+        return min+1;
+    }
+}
