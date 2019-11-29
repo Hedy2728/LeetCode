6e8e128b00d059d2d06454ef9061b30e4a6252f4
Hedy2728 <Haomeng2728@163.com>
2019-11-29 22:51:48 +0800

    for leetcode538

---
+++ src/leetcode538/Solution.java
@@ -0,0 +1,21 @@
+package leetcode538;
+
+import zju.hm.TreeNode;
+
+public class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        tranverse(root);
+        return root;
+    }
+
+    public void tranverse(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        tranverse(node.right);
+        sum += node.val;
+        node.val = sum;
+        tranverse(node.left);
+    }
+}
