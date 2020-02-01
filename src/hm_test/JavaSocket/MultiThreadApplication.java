74da330e4c371dc3198b0bf1d3a4c233d65a3a71
Hedy2728 <Haomeng2728@163.com>
2020-02-01 20:55:21 +0800

    java io

---
+++ src/hm_test/JavaSocket/MultiThreadApplication.java
@@ -0,0 +1,9 @@
+package hm_test.JavaSocket;
+
+public class MultiThreadApplication {
+    public static void main(String[] args) {
+        for (final String host:HttpConstant.HOSTS) {
+
+        }
+    }
+}
