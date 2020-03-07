53eba933cadfc3ae60485283877e6b4df8aea04e
Hedy2728 <Haomeng2728@163.com>
2020-03-07 21:26:59 +0800

    151

---
+++ src/leetcode151/Solution.java
@@ -0,0 +1,36 @@
+package leetcode151;
+
+import java.util.Stack;
+
+public class Solution {
+    public String reverseWords(String s) {
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+        s = s.trim();
+        int len = s.length();
+        int end = len;
+        StringBuilder sb = new StringBuilder();
+        for (int i = len - 1; i > 0; i--) {
+
+            if (s.charAt(i) == ' ') {
+                sb.append(s.substring(i + 1, end)).append(' ');
+                while (s.charAt(i - 1) == ' ') {
+                    i--;
+                }
+                end = i;
+            }
+        }
+        sb.append(s.substring(0, end));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "  hello   world!";
+        String[] ss = s.split(" ");
+        for (String t : ss) {
+            System.out.println(t);
+        }
+//        System.out.println(new Solution().reverseWords(s));
+    }
+}
